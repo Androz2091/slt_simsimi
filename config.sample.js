93553a12fad7a5f7faf808ee55edf88852536fb7
Androz2091 <androz2091@gmail.com>
2019-11-24 12:00:16 +0100

    Add sample config file

---
+++ config.sample.js
@@ -0,0 +1,9 @@
+module.exports = {
+    // Instagram credentials
+    username: "Instagram username",
+    password: "Instagram password",
+    // Simsimi options
+    key: "Simsimi api key",
+    lang: "fr",
+    atext_bad_prob_max: 0.3
+}
